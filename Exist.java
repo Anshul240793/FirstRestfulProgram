93782b8e91867ee0b740e8996e97c66855f4539d
anshul <anshul240793@gmail.com>
2020-01-28 18:24:01 +0530

    m1() message created

--- Exist.java
+++ Exist.java
@@ -1,12 +1,10 @@
 public class Exist{
-<<<<<<< HEAD
+
 public void m1(){
     System.out.println("m1");
  }
-=======
+
   public void m2(){
      System.out.println("m2"); 
   }
->>>>>>> 5d39deeac7c120f72aa9d9c2e04193dfff5bc5d5
-
 }
