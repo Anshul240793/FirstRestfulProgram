31003dbe6b3cb98c034a292d8a6accd7634db9b0
anshul <anshul240793@gmail.com>
2020-01-28 18:54:44 +0530

    bug 201 fixed

--- Exist.java
+++ Exist.java
@@ -7,8 +7,16 @@ public void m1(){
   public void m2(){
      System.out.println("m2"); 
   }
+
   //working on bug 207
   public void m4(){
       System .out.println("fixed bug 207");
+
+
+  //working in bug201
+  public void m3(){
+    //writing logic
+    int i =10;
+      System.out.println("bug 201 fixed");
   }
 }
